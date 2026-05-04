3ec47205f05654e8dc515ac5873e07965866705d
Manan Soni <64540033+manansoni77@users.noreply.github.com>
2026-05-04 16:28:02 +0530

    fix LLM client, update constants

--- backend/llm.py
+++ backend/llm.py
@@ -8,7 +8,6 @@ class LLMClient:
 
     #Stream completion for real-time response generation! We are on the right track 
     async def stream_completion(self, prompt, system_message, temperature=0.3):
-
         stream = await self.client.chat.completions.create(
             model="gpt-4o-mini",
             messages=[
@@ -19,51 +18,27 @@ class LLMClient:
             stream=True
         )
 
-        buffer = ""
-
         async for chunk in stream:
             if chunk.choices[0].delta.content:
-                buffer += chunk.choices[0].delta.content
-
-                # Flush on sentence end
-                if any(p in buffer for p in [".", "?", "!"]):
-                    yield buffer.strip()
-                    buffer = ""
-
-        if buffer.strip():
-            yield buffer.strip()
+                yield chunk.choices[0].delta.content
 
     # For structured data extraction, Passing JSON
-    async def get_json_response(self, prompt):
-
+    async def get_json_response(self, prompt, response_format = None):
         response = await self.client.chat.completions.create(
             model="gpt-4o-mini",
             messages=[{"role": "user", "content": prompt}],
-            temperature=0
+            temperature=0,
+            response_format=response_format # type: ignore
         )
 
-        content = response.choices[0].message.content
-
-        if not content:
-            return {
-                "summary": "",
-                "key_details": {},
-                "intent": "",
-                "missing_info": [],
-                "urgency_hint": 0.0,
-                "contradicts": False,
-                "human_requested": False
-            }
-
         try:
+            content = response.choices[0].message.content
+
+            if not content:
+                raise Exception("Empty response from LLM")
+            
             return json.loads(content)
-        except Exception:
-            return {
-                "summary": "",
-                "key_details": {},
-                "intent": "",
-                "missing_info": [],
-                "urgency_hint": 0.0,
-                "contradicts": False,
-                "human_requested": False
-            }
+
+        except Exception as e:
+            print("OpenAI json mode failed")
+            raise e
